c6ba41f368f08c09177686688a0910530510ba70
unknown <Mohammad>
2026-04-28 12:32:04 +0530

    Added lazy initialization

---
+++ src/main/java/journalApplication/Controller/GoogleAuthController.java
@@ -0,0 +1,98 @@
+package journalApplication.Controller;
+
+import journalApplication.Entity.User;
+import journalApplication.Repository.UserRepository;
+import journalApplication.Utils.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth/google")
+public class GoogleAuthController
+{
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String clientSecret;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private UserDetailsService userDetailsServiceImpl;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/callback")
+    public ResponseEntity<?> handleGoogleCallback(@RequestParam String code)
+    {
+        try {
+            String tokenEndPoint= "https://accounts.google.com/o/oauth2/token";
+            MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+            params.add("code", code);
+            params.add("client_id", clientId);
+            params.add("client_secret", clientSecret);
+            params.add("redirect_uri", "https://developers.google.com/oauthplayground");
+            params.add("grant_type", "authorization_code");
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+            ResponseEntity<Map> tokenResponse=restTemplate.postForEntity(tokenEndPoint,request,Map.class);
+            String idToken=(String) tokenResponse.getBody().get("id_token");
+            String userInfourl="https://oauth2.googleapis.com/tokeninfo?id_token=" +idToken;
+            ResponseEntity<Map> userInfoResponse=restTemplate.getForEntity(userInfourl,Map.class);
+
+            if (userInfoResponse.getStatusCode()== HttpStatus.OK)
+            {
+                Map<String,Object> userInfo=userInfoResponse.getBody();
+                String email=(String) userInfo.get("email");
+                UserDetails userDetails=null;
+                try {
+                    userDetails = userDetailsServiceImpl.loadUserByUsername(email);
+                }
+                catch (Exception e)
+                {
+                    User user=new User();
+                    user.setEmail(email);
+                    user.setUsername(email);
+                    user.setPassword(passwordEncoder.encode(UUID.randomUUID().toString()));
+                    user.setRole(Arrays.asList("USER"));
+                    userRepository.save(user);
+                }
+               String jwtToken=jwtUtil.generateToken(email);
+                return ResponseEntity.ok(Collections.singletonMap("token",jwtToken));
+            }
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        catch (Exception e){
+            log.error("Exception occured While handleGoogleCallback",e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+
+        }
+    }
+}
